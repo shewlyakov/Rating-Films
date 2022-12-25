ef27f28f9d6c7786cef7a32137228a92b29d56a5
shewlyakov <oleksandrsevlakov@gmail.com>
2022-12-25 15:39:20 +0200

    Fix rating system

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,44 +1,7 @@
 class ReviewsController < InheritedResources::Base
-  def index
-    @review = Review.all
-
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  def show
-    @review = Review.find(params[:id])
-
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  def new
-    @review = Review.new
-
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  def edit
-    @review = Review.find(params[:id])
-  end
-
   def create
-    @review = Review.new(review_params)
+    @review = current_user.reviews.new(review_params)
     @review.save
-    puts @review.errors.inspect
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to(@review, :notice => 'Review was successfully created.') }
-      else
-        format.html { render :action => "new" }
-      end
-    end
   end
 
   def update
@@ -53,18 +16,9 @@ class ReviewsController < InheritedResources::Base
     end
   end
 
-  def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(reviews_url) }
-    end
-  end
-
   private
 
   def review_params
-    params.require(:review).permit(:ratings, :film_id)
+    params.require(:review).permit(:ratings, :film_id, :user_id)
   end
 end
