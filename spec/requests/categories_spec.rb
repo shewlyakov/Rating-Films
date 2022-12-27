0adcfe80c781ab429e3e14e78559ca6cdb342a83
shewlyakov <oleksandrsevlakov@gmail.com>
2022-12-28 01:47:51 +0200

    Add requests test

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
+  let(:valid_attributes) { {title: 'Drama', image_url: 'category_drama.jpg'} }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "renders a successful response" do
+      Category.create! valid_attributes
+      get categories_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      category = Category.create! valid_attributes
+      get categories_url(category)
+      expect(response).to be_successful
+    end
   end
 end
