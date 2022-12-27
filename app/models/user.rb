51184e207ef2d1ce1fc5f7a42f0a49550821a314
shewlyakov <oleksandrsevlakov@gmail.com>
2022-12-27 23:28:26 +0200

    Test for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_many :reviews
 
-  validates :name, length: { minimum: 3 }, presence: true
-  validates :email, :encrypted_password, presence: true
-
+  validates_length_of :name, minimum: 3
+  validates_presence_of :name, :email, :encrypted_password
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should have_many(:reviews) }
+  end
+
+  describe "Validations" do
+    it { should validate_length_of(:name).is_at_least(3) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:encrypted_password) }
+  end
 end
